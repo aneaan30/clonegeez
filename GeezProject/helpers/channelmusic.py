2ad26d76a0f3d69f1c7f7ba28113116d27d271b3
aneaan30 <86281977+aneaan30@users.noreply.github.com>
2021-08-12 18:06:02 +0700

    Update channelmusic.py

--- GeezProject/helpers/channelmusic.py
+++ GeezProject/helpers/channelmusic.py
@@ -2,6 +2,6 @@ from pyrogram.types import Chat
 
 
 def get_chat_id(chat: Chat):
-    if chat.title.startswith("Channel Music: ") and chat.title[16:].isnumeric():
-        return int(chat.title[15:])
+    if chat.title.startswith("Channel Music: ") and chat.title[1:].isnumeric():
+        return int(chat.title[1:])
     return chat.id
